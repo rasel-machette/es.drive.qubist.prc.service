84cca30c19a8ade439ebba0ae5731b10d880dda7
rasel-machette <md.rasel@machette.tech>
2021-10-29 17:05:55 +0600

    first commit

--- src/main/java/es/drive/qubist/prc/service/drive/controller/KafkaController.java
+++ src/main/java/es/drive/qubist/prc/service/drive/controller/KafkaController.java
@@ -2,11 +2,15 @@ package es.drive.qubist.prc.service.drive.controller;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +24,18 @@ import es.drive.qubist.prc.service.drive.service.DriveService;
 
 @RestController
 public class KafkaController {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaController.class);
+
 	public static final String TOPIC = "Drive";
-	
-  @Autowired 
-  private KafkaTemplate<String, Object> kafkaTemp;
-	
-	@PostMapping(value = "/json/files", produces = { MediaType.APPLICATION_JSON_VALUE })
+
+	@Autowired
+	private KafkaTemplate<String, Object> kafkaTemplate;
+
+	@PostMapping(value = "/publish/files", produces = { MediaType.APPLICATION_JSON_VALUE })
 	@ResponseBody
 	public ResponseEntity<String> setFile(@RequestParam("file") MultipartFile file,
-			@RequestParam(name = "mime") String mimeType, @RequestParam(name = "parent", required = false) String parent) {
+			@RequestParam(name = "mime") String mime, @RequestParam(name = "parent", required = false) String parent) {
 		DriveService ds = null;
 		try {
 			ds = new DriveService();
@@ -44,13 +50,15 @@ public class KafkaController {
 		} catch (IOException | IllegalStateException e) {
 			e.printStackTrace();
 		}
-		File fileDrive = ds.setFile(tmpFile, mimeType, filename, parent);
-		System.out.println("Publishing to topic "+TOPIC);
-		this.kafkaTemp.send(TOPIC, tmpFile);
-		this.kafkaTemp.send(TOPIC, mimeType);
-		this.kafkaTemp.send(TOPIC, filename);
-		this.kafkaTemp.send(TOPIC, parent);
-		return new ResponseEntity<String>(HttpStatus.OK);
+		File fileDrive = ds.setFile(tmpFile, mime, filename, parent);
+		MultiValueMap<String, Object> data = new LinkedMultiValueMap<String, Object>();
+		data.add("tmpFile", tmpFile);
+		data.add("file", filename);
+		data.add("mime", mime);
+		data.add("parent", parent);
+		LOGGER.info("Publishing to topic " + TOPIC);
+		this.kafkaTemplate.send(TOPIC, data);
+		return new ResponseEntity<String>("Created", HttpStatus.OK);
 	}
-		
+
 }
--- src/main/java/es/drive/qubist/prc/service/drive/service/KafkaService.java
+++ src/main/java/es/drive/qubist/prc/service/drive/service/KafkaService.java
@@ -1,24 +1,16 @@
 package es.drive.qubist.prc.service.drive.service;
 
-import java.io.IOException;
-
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.google.api.services.drive.model.File;
-
 
 @Service
 public class KafkaService {
-	
-	@KafkaListener(topics = "Drive", groupId = "mygroup")
-	public void consumeMessage(String message) {
-		
-		System.out.println("Consummed message :"+message);
-		
-		
-	
+
+	@KafkaListener(topics = "OCR", groupId = "mygroup")
+	public void consumeMessage(String data) {
+
+		System.out.println("Consummed Data From OCR : " + data);
+
 	}
 
 }
